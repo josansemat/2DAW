6b5c5af00f33d63064532edf02ccdb0fc617f105
DESKTOP-NQO1TN8\josea <joseantonio.segura-matute@iesruizgijon.com>
2025-02-14 21:15:41 +0100

    Hasta capitulo 7

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -8,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto001';
+  nombre = 'Pepe';
+  edad = 21;
+  fumador = false;
+  sueldos=[1500, 2000, 3000];
+  provinccias=[
+    {id:1, nombre:'Madrid'},
+    {id:2, nombre:'Sevilla'},
+    {id:3, nombre:'Cadiz'},
+    {id:4, nombre:'Huelva'},
+    {id:5, nombre:'Barcelona'},
+  ];
+    esFumador(){
+      return this.fumador ? 'Es fumador' : 'No es fumador';
+    }
+    diHola(){
+      alert('Hola');
+    }
+    contador=0;
+    incremetar(){
+      this.contador++;
+    }
+    decrementar(){
+      this.contador--;
+    }
 }
