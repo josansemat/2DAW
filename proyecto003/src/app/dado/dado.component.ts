e5489f6ef0b29deb1d4276c5c41f5342dd8d4972
DESKTOP-NQO1TN8\josea <joseantonio.segura-matute@iesruizgijon.com>
2025-02-19 21:15:37 +0100

    hemos hecho un componente

--- proyecto003/src/app/dado/dado.component.ts
+++ proyecto003/src/app/dado/dado.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './dado.component.css'
 })
 export class DadoComponent {
+  valor!: number;
 
+  constructor() {
+    this.generaAleatorio();
+  }
+
+
+  generaAleatorio() {
+    this.valor = Math.floor(Math.random() * 6) + 1;
+  }
 }
