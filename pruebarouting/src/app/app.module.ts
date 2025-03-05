fb81a2e9157c3c46d7ae32eaa482ff48afe91b91
josansemat <joseantonio.segura-matute@iesruizgijon.com>
2025-03-05 17:31:47 +0100

    nuevo proyecto 404

---
+++ pruebarouting/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Pagina404Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
